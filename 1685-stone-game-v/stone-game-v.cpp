23bd42bbbea31b94723b42bd3b94b2e311945e7d
Girish Jain <163193398+girish-xd@users.noreply.github.com>
2026-08-17 13:09:43 +0530

    Time: 422 ms (55.85%) | Memory: 14.1 MB (98.65%) - LeetSync

---
+++ 1685-stone-game-v/stone-game-v.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    int memo[501][501];
+    int prefix[501];
+
+    int getSum(int i, int j) {
+        return prefix[j + 1] - prefix[i];
+    }
+
+    int solve(int i, int j, const std::vector<int>& stoneValue) {
+        if (i == j) return 0;
+        if (memo[i][j] != -1) return memo[i][j];
+
+        int maxScore = 0;
+
+        for (int k = i; k < j; ++k) {
+            int leftSum = getSum(i, k);
+            int rightSum = getSum(k + 1, j);
+
+            if (leftSum < rightSum) {
+                maxScore = std::max(maxScore, leftSum + solve(i, k, stoneValue));
+            } else if (leftSum > rightSum) {
+                maxScore = std::max(maxScore, rightSum + solve(k + 1, j, stoneValue));
+            } else {
+                maxScore = std::max(maxScore, leftSum + std::max(solve(i, k, stoneValue), solve(k + 1, j, stoneValue)));
+            }
+        }
+
+        return memo[i][j] = maxScore;
+    }
+public:
+    int stoneGameV(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        
+        prefix[0] = 0;
+        for (int i = 0; i < n; ++i) {
+            prefix[i + 1] = prefix[i] + stoneValue[i];
+        }
+
+        std::fill(&memo[0][0], &memo[0][0] + sizeof(memo) / sizeof(memo[0][0]), -1);
+
+        return solve(0, n - 1, stoneValue);
+    }
+};
